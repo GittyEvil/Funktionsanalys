f0f54b4b982711e1fe0ee6591f112cc5add20d0a
Adrian S <89584348+GittyEvil@users.noreply.github.com>
2024-11-19 16:18:51 +0100

    läs desc,gäller även ui.c
    Håller fortf på med benchmark fast nu mer med tidräkning och implementation av den. Har även fixat UI(front end) lite för att kunna börja testa mig fram då jag tror jag kan börja för back end är typ klar

--- analyze.c
+++ analyze.c
@@ -1,5 +1,10 @@
 #include "analyze.h"
 #include "algorithm.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define sizing 100
 
 //
 // Private
@@ -21,6 +26,31 @@ static void generateArray(int *arr, int n, case_t c) {
     }
 }
 
+int scanfnPrint(int value) {
+    printf("välj ett value: ");
+    scanf("%d",&value);
+    return value;
+}
+
+//för att minska upprepningar gällande tidmätning
+void timeInit( void (*funct)()) {
+    //clock_t start,end;
+    struct timespec *start;
+    struct timespec *end;
+    
+    //ska ta tid från innan funktion körs tills den är klar sen returnera ut in i *buf
+    //time start
+    clock_gettime(CLOCK_MONOTONIC,&start);
+    
+    funct():
+    
+    //time end;
+    clock_gettime(CLOCK_MONOTONIC,&end);
+    
+    //return time;
+    return end.tv_nsec - start.tv_nsec;
+}
+
 //
 // Public
 //
@@ -34,21 +64,24 @@ void benchmark(const algorithm_t a, const case_t c, result_t *buf, int n)
     så det som behövs är nog:
     switch case för varje algoritm följer då parameter a
     funktion för att fylla array, vet ej storlek så behöver förmodligen kunna ha allokering för att göra det dynamiskt
+
+    kunna printa ut informationen efter varje körning är också något som behövs implementeras, alltså tidskvantor
     
     parameter c är för vilken typ av tidstest vi vill ha alltså bästa, sämsta eller det genomsnittliga
     */
 
     //behöver allokera upp plats för array och size
-    int size = NULL;
+    int size = sizing;
     int* array = (int*) malloc(size *sizeof(int));
-
+    int value = 0;
     generateArray(array,size,c);
     
     for(int i = 0; i < n;i++) {
         switch(a) {
             case bubble_sort_t:
                 //Bubblesort
-                bubble_sort(array,size);
+                //bubble_sort(array,size);
+                timeInit(bubble_sort(array,size));
                 break;
 
             case insertion_sort_t:
@@ -63,17 +96,15 @@ void benchmark(const algorithm_t a, const case_t c, result_t *buf, int n)
 
             case linear_search_t:
                 //linear
+                value = scanfnPrint(value);
                 linear_search(array,size,value);
                 break;
 
             case binary_search_t:
                 //binary
+                value = scanfnPrint(value);
                 binary_search(array,size,value);
                 break;
-
-            
         }
-        
-        
     }
 }
